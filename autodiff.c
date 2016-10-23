5ca930636a6b3fb8249087965064484028b515ed
Heng Li <lh3@me.com>
2016-10-23 14:51:17 -0400

    tree linearization working

--- autodiff.c
+++ autodiff.c
@@ -1,93 +1,162 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 #include "autodiff.h"
 
-struct ad_tnode_t {
+#define AD_OP_VAR      0
+#define AD_OP_ADD      1
+#define AD_OP_SUB      2
+#define AD_OP_MUL      3   // matrix product
+#define AD_OP_SMUL     4   // scalar-matrix product
+#define AD_OP_HMUL     5   // Hadamard product
+#define AD_OP_DOT      6   // vector dot (inner product)
+#define AD_OP_SQUARE   101
+#define AD_OP_SIGM     102
+
+/***************************
+ * Expression construction *
+ ***************************/
+
+struct ad_node_t {
 	int n_row, n_col;
 	int n_child, op;
 	const float *x;
-	struct ad_tnode_t *parent, **child;
+	struct ad_node_t **child;
 };
 
-static inline ad_tnode_t *ad_new_core(int n_row, int n_col, int n_child, const float *x)
+static inline ad_node_t *ad_new_core(int n_row, int n_col, int n_child, const float *x)
 {
-	ad_tnode_t *s;
-	s = (ad_tnode_t*)calloc(1, sizeof(ad_tnode_t));
+	ad_node_t *s;
+	s = (ad_node_t*)calloc(1, sizeof(ad_node_t));
 	s->n_row = n_row, s->n_col = n_col, s->x = x;
 	s->n_child = n_child;
-	if (s->n_child) s->child = (ad_tnode_t**)calloc(s->n_child, sizeof(ad_tnode_t*));
+	if (s->n_child) s->child = (ad_node_t**)calloc(s->n_child, sizeof(ad_node_t*));
 	return s;
 }
 
-ad_tnode_t *ad_new(int n_row, int n_col, const float *x)
+ad_node_t *ad_bind(int n_row, int n_col, const float *x)
 {
 	return ad_new_core(n_row, n_col, 0, x);
 }
 
-void ad_del(ad_tnode_t *s)
+static inline ad_node_t *ad_op2_core(int op, int n_row, int n_col, ad_node_t *x, ad_node_t *y)
 {
-	free(s->child);
-	free(s);
-}
-
-static inline ad_tnode_t *ad_op1_core(int op, int n_row, int n_col, ad_tnode_t *x)
-{
-	ad_tnode_t *s;
-	s = ad_new_core(n_row, n_col, 1, 0);
-	s->op = op;
-	s->child[0] = x;
-	x->parent = s;
-	return s;
-}
-
-static inline ad_tnode_t *ad_op2_core(int op, int n_row, int n_col, ad_tnode_t *x, ad_tnode_t *y)
-{
-	ad_tnode_t *s;
+	ad_node_t *s;
 	s = ad_new_core(n_row, n_col, 2, 0);
 	s->op = op;
 	s->child[0] = x, s->child[1] = y;
-	x->parent = y->parent = s;
 	return s;
 }
 
-ad_tnode_t *ad_add(ad_tnode_t *x, ad_tnode_t *y)
+#define AD_FUNC_OP2(fname, op, cond, _row, _col) \
+	ad_node_t *fname(ad_node_t *x, ad_node_t *y) { return (cond)? 0 : ad_op2_core((op), (_row), (_col), x, y); }
+
+AD_FUNC_OP2(ad_add, AD_OP_ADD, (x->n_row != y->n_row || x->n_col != y->n_col), x->n_row, x->n_col)
+AD_FUNC_OP2(ad_sub, AD_OP_SUB, (x->n_row != y->n_row || x->n_col != y->n_col), x->n_row, x->n_col)
+AD_FUNC_OP2(ad_hmul, AD_OP_HMUL, (x->n_row != y->n_row || x->n_col != y->n_col), x->n_row, x->n_col)
+AD_FUNC_OP2(ad_mul, AD_OP_MUL, (x->n_col != y->n_row), x->n_row, y->n_col)
+AD_FUNC_OP2(ad_smul, AD_OP_SMUL, (x->n_row == 1 && x->n_col == 1), y->n_row, y->n_col)
+AD_FUNC_OP2(ad_dot, AD_OP_DOT, (x->n_row != y->n_row || x->n_col != y->n_col), 1, x->n_col)
+
+static inline ad_node_t *ad_op1_core(int op, int n_row, int n_col, ad_node_t *x)
 {
-	return (x->n_row != y->n_row || x->n_col != y->n_col)? 0 : ad_op2_core(AD_OP_ADD, x->n_row, x->n_col, x, y);
+	ad_node_t *s;
+	s = ad_new_core(n_row, n_col, 1, 0);
+	s->op = op;
+	s->child[0] = x;
+	return s;
 }
 
-ad_tnode_t *ad_sub(ad_tnode_t *x, ad_tnode_t *y)
+#define AD_FUNC_OP1(fname, op, _row, _col) \
+	ad_node_t *fname(ad_node_t *x) { return ad_op1_core((op), (_row), (_col), x); }
+
+AD_FUNC_OP1(ad_square, AD_OP_SQUARE, 1, x->n_col)
+AD_FUNC_OP1(ad_sigm, AD_OP_SIGM, x->n_row, x->n_col)
+
+/**************************
+ * Expression compilation *
+ **************************/
+
+#define kvec_t(type) struct { size_t n, m; type *a; }
+
+#define kv_pop(v) ((v).a[--(v).n])
+#define kv_pushp(type, v, p) do { \
+		if ((v).n == (v).m) { \
+			(v).m = (v).m? (v).m<<1 : 2; \
+			(v).a = (type*)realloc((v).a, sizeof(type) * (v).m); \
+		} \
+		*(p) = &(v).a[(v).n++]; \
+	} while (0)
+
+typedef struct {
+	int op, n_operand, n_row, n_col;
+	union {
+		const float *cx;
+		float *x;
+	} _;
+} ad_expr1_t;
+
+struct ad_expr_t {
+	int32_t n;
+	ad_expr1_t *a;
+};
+
+typedef struct {
+	ad_node_t *p;
+	int n;
+} ad_tnode_t;
+
+ad_expr_t *ad_expr_compile(ad_node_t *root)
 {
-	return (x->n_row != y->n_row || x->n_col != y->n_col)? 0 : ad_op2_core(AD_OP_SUB, x->n_row, x->n_col, x, y);
+	kvec_t(ad_tnode_t) stack = {0,0,0};
+	kvec_t(ad_expr1_t) expr = {0,0,0};
+	ad_tnode_t *p;
+	ad_expr1_t *q;
+	ad_expr_t *e;
+
+	kv_pushp(ad_tnode_t, stack, &p);
+	p->p = root, p->n = root->n_child;
+	while (stack.n) {
+		ad_tnode_t *t = &stack.a[stack.n-1];
+		if (t->n == 0) {
+			kv_pushp(ad_expr1_t, expr, &q);
+			q->op = t->p->op;
+			q->n_operand = t->p->n_child;
+			q->n_row = t->p->n_row, q->n_col = t->p->n_col;
+			q->_.cx = t->p->x;
+			free(t->p->child);
+			free(t->p);
+			--stack.n;
+		} else {
+			--t->n;
+			kv_pushp(ad_tnode_t, stack, &p);
+			p->p = t->p->child[t->n], p->n = p->p->n_child;
+		}
+	}
+	free(stack.a);
+	e = (ad_expr_t*)calloc(1, sizeof(ad_expr_t));
+	e->n = expr.n;
+	e->a = expr.a;
+	return e;
 }
 
-ad_tnode_t *ad_hmul(ad_tnode_t *x, ad_tnode_t *y)
+void ad_expr_destroy(ad_expr_t *e)
 {
-	return (x->n_row != y->n_row || x->n_col != y->n_col)? 0 : ad_op2_core(AD_OP_HMUL, x->n_row, x->n_col, x, y);
+	int32_t i;
+	for (i = 0; i < e->n; ++i) {
+		if (e->a[i].op != AD_OP_VAR)
+			free(e->a[i]._.x);
+	}
+	free(e->a);
+	free(e);
 }
 
-ad_tnode_t *ad_mul(ad_tnode_t *x, ad_tnode_t *y)
+void ad_expr_debug(const ad_expr_t *e)
 {
-	return (x->n_col != y->n_row)? 0 : ad_op2_core(AD_OP_MUL, x->n_row, y->n_col, x, y);
-}
-
-ad_tnode_t *ad_smul(ad_tnode_t *x, ad_tnode_t *y)
-{
-	if (x->n_row == 1 && x->n_col == 1) return ad_op2_core(AD_OP_SMUL, y->n_row, y->n_col, x, y);
-	if (y->n_row == 1 && y->n_col == 1) return ad_op2_core(AD_OP_SMUL, x->n_row, x->n_col, y, x);
-	return 0;
-}
-
-ad_tnode_t *ad_dot(ad_tnode_t *x, ad_tnode_t *y)
-{
-	return (x->n_row != y->n_row || x->n_col != y->n_col)? 0 : ad_op2_core(AD_OP_DOT, 1, x->n_col, x, y);
-}
-
-ad_tnode_t *ad_square(ad_tnode_t *x)
-{
-	return ad_op1_core(AD_OP_SQUARE, 1, x->n_col, x);
-}
-
-ad_tnode_t *ad_sigm(ad_tnode_t *x)
-{
-	return ad_op1_core(AD_OP_SIGM, x->n_row, x->n_col, x);
+	int32_t i;
+	for (i = 0; i < e->n; ++i) {
+		if (i > 0) printf(" ");
+		printf("%d,%d", e->a[i].op, e->a[i].n_operand);
+	}
+	putchar('\n');
 }
--- autodiff.h
+++ autodiff.h
@@ -1,31 +1,34 @@
 #ifndef KANN_AUTODIFF_H
 #define KANN_AUTODIFF_H
 
-#define AD_OP_ADD      1
-#define AD_OP_SUB      2
-#define AD_OP_MUL      3   // matrix product
-#define AD_OP_SMUL     4   // scalar-matrix product
-#define AD_OP_HMUL     5   // Hadamard product
-#define AD_OP_DOT      6   // vector dot (inner product)
-#define AD_OP_SQUARE   101
-#define AD_OP_SIGM     102
+#include <stdint.h>
 
-struct ad_tnode_t;
-typedef struct ad_tnode_t ad_tnode_t;
+struct ad_node_t;
+typedef struct ad_node_t ad_node_t;
+
+struct ad_expr_t;
+typedef struct ad_expr_t ad_expr_t;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-ad_tnode_t *ad_new(int n_row, int n_col, const float *x);
-ad_tnode_t *ad_add(ad_tnode_t *x, ad_tnode_t *y);
-ad_tnode_t *ad_sub(ad_tnode_t *x, ad_tnode_t *y);
-ad_tnode_t *ad_hmul(ad_tnode_t *x, ad_tnode_t *y);
-ad_tnode_t *ad_mul(ad_tnode_t *x, ad_tnode_t *y);
-ad_tnode_t *ad_smul(ad_tnode_t *x, ad_tnode_t *y);
-ad_tnode_t *ad_dot(ad_tnode_t *x, ad_tnode_t *y);
-ad_tnode_t *ad_square(ad_tnode_t *x);
-ad_tnode_t *ad_sigm(ad_tnode_t *x);
+ad_node_t *ad_bind(int n_row, int n_col, const float *x);
+
+ad_node_t *ad_add(ad_node_t *x, ad_node_t *y);
+ad_node_t *ad_sub(ad_node_t *x, ad_node_t *y);
+ad_node_t *ad_hmul(ad_node_t *x, ad_node_t *y);
+ad_node_t *ad_mul(ad_node_t *x, ad_node_t *y);
+ad_node_t *ad_smul(ad_node_t *x, ad_node_t *y);
+ad_node_t *ad_dot(ad_node_t *x, ad_node_t *y);
+
+ad_node_t *ad_square(ad_node_t *x);
+ad_node_t *ad_sigm(ad_node_t *x);
+
+ad_expr_t *ad_expr_compile(ad_node_t *root);
+void ad_expr_eval(const ad_expr_t *e);
+void ad_expr_destroy(ad_expr_t *e);
+void ad_expr_debug(const ad_expr_t *e);
 
 #ifdef __cplusplus
 }
