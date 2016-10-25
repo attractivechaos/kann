283b628fc994421c82b28f135651c7925daf3c64
Heng Li <lh3@me.com>
2016-10-25 10:19:22 -0400

    reordering functions to highlight important ones

--- autodiff.c
+++ autodiff.c
@@ -5,91 +5,6 @@
 #include <math.h>
 #include "autodiff.h"
 
-/***************************
- * Basic vector operations *
- ***************************/
-
-#define kvec_t(type) struct { size_t n, m; type *a; }
-
-#define kv_pop(v) ((v).a[--(v).n])
-
-#define kv_push(type, v, x) do { \
-		if ((v).n == (v).m) { \
-			(v).m = (v).m? (v).m<<1 : 2; \
-			(v).a = (type*)realloc((v).a, sizeof(type) * (v).m); \
-		} \
-		(v).a[(v).n++] = (x); \
-	} while (0)
-
-/*********************
- * Vector operations *
- *********************/
-
-#ifdef __SSE__
-#include <xmmintrin.h>
-
-float ad_vec_sdot(int n, const float *x, const float *y)
-{
-	int i, n8 = n>>3<<3;
-	__m128 vs1, vs2;
-	float s, t[4];
-	vs1 = _mm_setzero_ps();
-	vs2 = _mm_setzero_ps();
-	for (i = 0; i < n8; i += 8) {
-		__m128 vx1, vx2, vy1, vy2;
-		vx1 = _mm_loadu_ps(&x[i]);
-		vx2 = _mm_loadu_ps(&x[i+4]);
-		vy1 = _mm_loadu_ps(&y[i]);
-		vy2 = _mm_loadu_ps(&y[i+4]);
-		vs1 = _mm_add_ps(vs1, _mm_mul_ps(vx1, vy1));
-		vs2 = _mm_add_ps(vs2, _mm_mul_ps(vx2, vy2));
-	}
-	for (s = 0.; i < n; ++i) s += x[i] * y[i];
-	_mm_storeu_ps(t, vs1);
-	s += t[0] + t[1] + t[2] + t[3];
-	_mm_storeu_ps(t, vs2);
-	s += t[0] + t[1] + t[2] + t[3];
-	return s;
-}
-void ad_vec_saxpy(int n, float a, const float *x, float *y)
-{
-	int i, n8 = n>>3<<3;
-	__m128 va;
-	va = _mm_set1_ps(a);
-	for (i = 0; i < n8; i += 8) {
-		__m128 vx1, vx2, vy1, vy2, vt1, vt2;
-		vx1 = _mm_loadu_ps(&x[i]);
-		vx2 = _mm_loadu_ps(&x[i+4]);
-		vy1 = _mm_loadu_ps(&y[i]);
-		vy2 = _mm_loadu_ps(&y[i+4]);
-		vt1 = _mm_add_ps(_mm_mul_ps(va, vx1), vy1);
-		vt2 = _mm_add_ps(_mm_mul_ps(va, vx2), vy2);
-		_mm_storeu_ps(&y[i], vt1);
-		_mm_storeu_ps(&y[i+4], vt2);
-	}
-	for (; i < n; ++i) y[i] += a * x[i];
-}
-#else
-void ad_vec_saxpy(int n, float a, const float *x, float *y) // BLAS saxpy
-{
-	int i;
-	for (i = 0; i < n; ++i) y[i] += a * x[i];
-}
-float ad_vec_sdot(int n, const float *x, const float *y) // BLAS sdot
-{
-	int i;
-	float s = 0.;
-	for (i = 0; i < n; ++i) s += x[i] * y[i];
-	return s;
-}
-#endif
-
-void ad_vec_elem_mul(int n, const float *x, const float *y, float *z)
-{
-	int i;
-	for (i = 0; i < n; ++i) z[i] += x[i] * y[i];
-}
-
 /**********************
  * Graph construction *
  **********************/
@@ -144,6 +59,18 @@ AD_FUNC_OP1(ad_tanh, 11, x->n_row, x->n_col)
 /*******************
  * Graph traversal *
  *******************/
+
+#define kvec_t(type) struct { size_t n, m; type *a; }
+
+#define kv_pop(v) ((v).a[--(v).n])
+
+#define kv_push(type, v, x) do { \
+		if ((v).n == (v).m) { \
+			(v).m = (v).m? (v).m<<1 : 2; \
+			(v).a = (type*)realloc((v).a, sizeof(type) * (v).m); \
+		} \
+		(v).a[(v).n++] = (x); \
+	} while (0)
 
 typedef struct ad_node_t *ad_node_p;
 
@@ -215,6 +142,10 @@ void ad_free(int n, ad_node_t **a)
  * Automatic differentiation *
  *****************************/
 
+void ad_vec_saxpy(int n, float a, const float *x, float *y); // BLAS saxpy
+float ad_vec_sdot(int n, const float *x, const float *y); // BLAS sdot
+void ad_vec_elem_mul(int n, const float *x, const float *y, float *z);
+
 typedef void (*ad_op_f)(struct ad_node_t*);
 
 static ad_op_f ad_op_list[]; // actual operators are defined and implemented at the end of this source file
@@ -275,6 +206,75 @@ float ad_eval(int n, ad_node_t **a)
 	fret = ad_forward(n, a);
 	ad_backward(n, a);
 	return fret;
+}
+
+/*********************
+ * Vector operations *
+ *********************/
+
+#ifdef __SSE__
+#include <xmmintrin.h>
+
+float ad_vec_sdot(int n, const float *x, const float *y)
+{
+	int i, n8 = n>>3<<3;
+	__m128 vs1, vs2;
+	float s, t[4];
+	vs1 = _mm_setzero_ps();
+	vs2 = _mm_setzero_ps();
+	for (i = 0; i < n8; i += 8) {
+		__m128 vx1, vx2, vy1, vy2;
+		vx1 = _mm_loadu_ps(&x[i]);
+		vx2 = _mm_loadu_ps(&x[i+4]);
+		vy1 = _mm_loadu_ps(&y[i]);
+		vy2 = _mm_loadu_ps(&y[i+4]);
+		vs1 = _mm_add_ps(vs1, _mm_mul_ps(vx1, vy1));
+		vs2 = _mm_add_ps(vs2, _mm_mul_ps(vx2, vy2));
+	}
+	for (s = 0.; i < n; ++i) s += x[i] * y[i];
+	_mm_storeu_ps(t, vs1);
+	s += t[0] + t[1] + t[2] + t[3];
+	_mm_storeu_ps(t, vs2);
+	s += t[0] + t[1] + t[2] + t[3];
+	return s;
+}
+void ad_vec_saxpy(int n, float a, const float *x, float *y)
+{
+	int i, n8 = n>>3<<3;
+	__m128 va;
+	va = _mm_set1_ps(a);
+	for (i = 0; i < n8; i += 8) {
+		__m128 vx1, vx2, vy1, vy2, vt1, vt2;
+		vx1 = _mm_loadu_ps(&x[i]);
+		vx2 = _mm_loadu_ps(&x[i+4]);
+		vy1 = _mm_loadu_ps(&y[i]);
+		vy2 = _mm_loadu_ps(&y[i+4]);
+		vt1 = _mm_add_ps(_mm_mul_ps(va, vx1), vy1);
+		vt2 = _mm_add_ps(_mm_mul_ps(va, vx2), vy2);
+		_mm_storeu_ps(&y[i], vt1);
+		_mm_storeu_ps(&y[i+4], vt2);
+	}
+	for (; i < n; ++i) y[i] += a * x[i];
+}
+#else
+void ad_vec_saxpy(int n, float a, const float *x, float *y) // BLAS saxpy
+{
+	int i;
+	for (i = 0; i < n; ++i) y[i] += a * x[i];
+}
+float ad_vec_sdot(int n, const float *x, const float *y) // BLAS sdot
+{
+	int i;
+	float s = 0.;
+	for (i = 0; i < n; ++i) s += x[i] * y[i];
+	return s;
+}
+#endif
+
+void ad_vec_elem_mul(int n, const float *x, const float *y, float *z)
+{
+	int i;
+	for (i = 0; i < n; ++i) z[i] += x[i] * y[i];
 }
 
 /*************
