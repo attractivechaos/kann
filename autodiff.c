1374566bd99667af3ce95c7e49412a4127362551
Heng Li <lh3@me.com>
2016-10-26 13:00:16 -0400

    a bit code cleanup; added smul()

--- autodiff.c
+++ autodiff.c
@@ -62,13 +62,14 @@ AD_FUNC_OP2(ad_add, 1)
 AD_FUNC_OP2(ad_sub, 2)
 AD_FUNC_OP2(ad_mul, 3)
 AD_FUNC_OP2(ad_mtmul, 4)
-AD_FUNC_OP2(ad_ce2, 8)
+AD_FUNC_OP2(ad_smul, 5)
+AD_FUNC_OP2(ad_ce2, 6)
 
 #define AD_FUNC_OP1(fname, op) ad_node_t *fname(ad_node_t *x) { return ad_op1_core((op), x); }
 
-AD_FUNC_OP1(ad_norm2, 9)
-AD_FUNC_OP1(ad_sigm, 10)
-AD_FUNC_OP1(ad_tanh, 11)
+AD_FUNC_OP1(ad_norm2, 7)
+AD_FUNC_OP1(ad_sigm, 8)
+AD_FUNC_OP1(ad_tanh, 9)
 
 /*****************************
  * Automatic differentiation *
@@ -189,7 +190,7 @@ float ad_eval(int n, ad_node_t **a)
 #ifdef __SSE__
 #include <xmmintrin.h>
 
-float ad_vec_sdot(int n, const float *x, const float *y)
+float ad_sdot(int n, const float *x, const float *y)
 {
 	int i, n8 = n>>3<<3;
 	__m128 vs1, vs2;
@@ -212,7 +213,7 @@ float ad_vec_sdot(int n, const float *x, const float *y)
 	s += t[0] + t[1] + t[2] + t[3];
 	return s;
 }
-void ad_vec_saxpy(int n, float a, const float *x, float *y)
+void ad_saxpy(int n, float a, const float *x, float *y)
 {
 	int i, n8 = n>>3<<3;
 	__m128 va;
@@ -230,26 +231,41 @@ void ad_vec_saxpy(int n, float a, const float *x, float *y)
 	}
 	for (; i < n; ++i) y[i] += a * x[i];
 }
-#else
-void ad_vec_saxpy(int n, float a, const float *x, float *y) // BLAS saxpy
+void ad_vec_mul(int n, float *x, const float *y)
 {
-	int i;
-	for (i = 0; i < n; ++i) y[i] += a * x[i];
+	int i, n8 = n>>3<<3;
+	for (i = 0; i < n8; i += 8) {
+		__m128 vx1, vx2, vy1, vy2, vt1, vt2;
+		vx1 = _mm_loadu_ps(&x[i]);
+		vx2 = _mm_loadu_ps(&x[i+4]);
+		vy1 = _mm_loadu_ps(&y[i]);
+		vy2 = _mm_loadu_ps(&y[i+4]);
+		vt1 = _mm_mul_ps(vx1, vy1);
+		vt2 = _mm_mul_ps(vx2, vy2);
+		_mm_storeu_ps(&x[i], vt1);
+		_mm_storeu_ps(&x[i+4], vt2);
+	}
+	for (; i < n; ++i) x[i] *= y[i];
 }
-float ad_vec_sdot(int n, const float *x, const float *y) // BLAS sdot
+#else
+float ad_sdot(int n, const float *x, const float *y) // BLAS sdot
 {
 	int i;
 	float s = 0.;
 	for (i = 0; i < n; ++i) s += x[i] * y[i];
 	return s;
 }
-#endif
-
-void ad_vec_elem_mul(int n, const float *x, const float *y, float *z)
+void ad_saxpy(int n, float a, const float *x, float *y) // BLAS saxpy
 {
 	int i;
-	for (i = 0; i < n; ++i) z[i] += x[i] * y[i];
+	for (i = 0; i < n; ++i) y[i] += a * x[i];
 }
+void ad_vec_mul(int n, float *a, const float *b)
+{
+	int i;
+	for (i = 0; i < n; ++i) a[i] *= b[i];
+}
+#endif
 
 void ad_mat_mtmul(int n_col, int n_a_row, const float *a, int n_b_row, const float *b, float *c) // C = A * B^T
 {
@@ -261,7 +277,7 @@ void ad_mat_mtmul(int n_col, int n_a_row, const float *a, int n_b_row, const flo
 			int jj, je = n_b_row < j + x? n_b_row : j + x;
 			for (ii = i; ii < ie; ++ii)
 				for (jj = j; jj < je; ++jj)
-					c[ii*n_b_row+jj] += ad_vec_sdot(n_col, &a[ii*n_col], &b[jj*n_col]);
+					c[ii*n_b_row+jj] += ad_sdot(n_col, &a[ii*n_col], &b[jj*n_col]);
 		}
 	}
 }
@@ -282,7 +298,7 @@ int ad_op_add(ad_node_t *p, int action)
 		p->n_row = e[0]->p->n_row, p->n_col = e[0]->p->n_col;
 	} else if (action == AD_FORWARD) {
 		memcpy(p->_.x, e[0]->p->_.x, n * sizeof(float));
-		ad_vec_saxpy(n, 1.0f, e[1]->p->_.x, p->_.x);
+		ad_saxpy(n, 1.0f, e[1]->p->_.x, p->_.x);
 	} else if (action == AD_BACKWARD) {
 		if (e[0]->p->to_back) memcpy(e[0]->p->d, p->d, n * sizeof(float));
 		if (e[1]->p->to_back) memcpy(e[1]->p->d, p->d, n * sizeof(float));
@@ -302,7 +318,7 @@ int ad_op_sub(ad_node_t *p, int action)
 		p->n_row = e[0]->p->n_row, p->n_col = e[0]->p->n_col;
 	} else if (action == AD_FORWARD) {
 		memcpy(p->_.x, e[0]->p->_.x, n * sizeof(float));
-		ad_vec_saxpy(n, -1.0f, e[1]->p->_.x, p->_.x);
+		ad_saxpy(n, -1.0f, e[1]->p->_.x, p->_.x);
 	} else if (action == AD_BACKWARD) {
 		if (e[0]->p->to_back) memcpy(e[0]->p->d, p->d, n * sizeof(float));
 		if (e[1]->p->to_back) {
@@ -325,12 +341,37 @@ int ad_op_mul(ad_node_t *p, int action)
 		if (e[0]->p->n_row != e[1]->p->n_row || e[0]->p->n_col != e[1]->p->n_col) return -1;
 		p->n_row = e[0]->p->n_row, p->n_col = e[0]->p->n_col;
 	} else if (action == AD_FORWARD) {
-		memset(p->_.x, 0, n * sizeof(float));
-		ad_vec_elem_mul(n, e[0]->p->_.x, e[1]->p->_.x, p->_.x);
+		memcpy(p->_.x, e[0]->p->_.x, n * sizeof(float));
+		ad_vec_mul(n, p->_.x, e[1]->p->_.x);
 	} else if (action == AD_BACKWARD) {
 		for (i = 0; i < 2; ++i)
-			if (e[i]->p->to_back)
-				ad_vec_elem_mul(n, p->d, e[!i]->p->_.x, e[i]->p->d);
+			if (e[i]->p->to_back) {
+				memcpy(e[i]->p->d, p->d, n * sizeof(float));
+				ad_vec_mul(n, e[i]->p->d, e[!i]->p->_.x);
+			}
+	}
+	return 0;
+}
+
+int ad_op_smul(ad_node_t *p, int action)
+{
+	int n = p->n_row * p->n_col;
+	ad_edge_t *e[2];
+
+	e[0] = &p->child[0];
+	e[1] = &p->child[1];
+	assert(e[0]->p->to_back == 0);
+	if (action == AD_SYNCDIM) {
+		if (e[0]->p->n_row != 1 || e[0]->p->n_col != 1) return -1;
+		p->n_row = e[1]->p->n_row, p->n_col = e[1]->p->n_col;
+	} else if (action == AD_FORWARD) {
+		memset(p->_.x, 0, n * sizeof(float));
+		ad_saxpy(n, e[0]->p->_.x[0], e[1]->p->_.x, p->_.x);
+	} else if (action == AD_BACKWARD) {
+		if (e[1]->p->to_back) {
+			memset(e[1]->p->d, 0, n * sizeof(float));
+			ad_saxpy(n, e[1]->p->_.x[0], p->d, e[1]->p->d);
+		}
 	}
 	return 0;
 }
@@ -354,15 +395,11 @@ int ad_op_mtmul(ad_node_t *p, int action)
 			int i, j, n_col = e[0]->p->n_col;
 			for (i = 0; i < e[0]->p->n_row; ++i)
 				for (j = 0; j < e[1]->p->n_row; ++j)
-					ad_vec_saxpy(n_col, p->d[i * e[1]->p->n_row + j], e[0]->p->_.x + i * n_col, e[1]->p->d + j * n_col);
+					ad_saxpy(n_col, p->d[i * e[1]->p->n_row + j], e[0]->p->_.x + i * n_col, e[1]->p->d + j * n_col);
 		}
 	}
 	return 0;
 }
-
-int ad_op_smul(ad_node_t *p, int action) { return 0; }
-int ad_op_dot(ad_node_t *p, int action) { return 0; }
-int ad_op_div(ad_node_t *p, int action) { return 0; }
 
 int ad_op_ce2(ad_node_t *p, int action)
 {
@@ -393,7 +430,7 @@ int ad_op_ce2(ad_node_t *p, int action)
 	} else if (action == AD_BACKWARD) {
 		if (e[0]->p->to_back) {
 			memset(e[0]->p->d, 0, n * sizeof(float));
-			ad_vec_saxpy(n, p->d[0], e[0]->t, e[0]->p->d);
+			ad_saxpy(n, p->d[0], e[0]->t, e[0]->p->d);
 		}
 	}
 	return 0;
@@ -406,11 +443,11 @@ int ad_op_norm2(ad_node_t *p, int action)
 	if (action == AD_SYNCDIM) {
 		p->n_row = p->n_col = 1;
 	} else if (action == AD_FORWARD) {
-		p->_.x[0] = ad_vec_sdot(n, e->p->_.x, e->p->_.x);
+		p->_.x[0] = ad_sdot(n, e->p->_.x, e->p->_.x);
 	} else if (action == AD_BACKWARD) {
 		if (e->p->to_back) {
 			memcpy(e->p->d, e->p->_.x, n * sizeof(float));
-			ad_vec_saxpy(n, 1.0f, e->p->_.x, e->p->d);
+			ad_saxpy(n, 1.0f, e->p->_.x, e->p->d);
 		}
 	}
 	return 0;
@@ -442,7 +479,7 @@ int ad_op_tanh(ad_node_t *p, int action)
 	} else if (action == AD_FORWARD) {
 		for (i = 0; i < n; ++i) {
 			float y;
-			y = expf(2.0f * e->p->_.x[i]);
+			y = expf(-2.0f * e->p->_.x[i]);
 			p->_.x[i] = (1.0f - y) / (1.0f + y);
 		}
 	} else if (action == AD_BACKWARD) {
@@ -455,16 +492,14 @@ int ad_op_tanh(ad_node_t *p, int action)
 
 ad_op_f ad_op_list[] = {
 	0,
-	ad_op_add,     // 1: addition
-	ad_op_sub,     // 2: subtraction
-	ad_op_mul,     // 3: element-wise product
-	ad_op_mtmul,   // 4: matrix product
-	ad_op_smul,    // 5: scalar-to-matrix product
-	ad_op_dot,     // 6: vector dot/inner product
-	ad_op_div,     // 7: element-wise division
-	ad_op_ce2,     // 8: binary cross-entropy: F(x,y) = \sum_i -y_i*log(f(x_i)) - (1-y_i)*log(1-f(x_i)); f() is sigmoid
-	ad_op_norm2,   // 9: ||x|| = x^T x
-	ad_op_sigm,    // 10: element-wise sigmoind function
-	ad_op_tanh,     // 11: tanh
+	ad_op_add,
+	ad_op_sub,
+	ad_op_mul,
+	ad_op_mtmul,
+	ad_op_smul,
+	ad_op_ce2,
+	ad_op_norm2,
+	ad_op_sigm,
+	ad_op_tanh,
 	0
 };
--- autodiff.h
+++ autodiff.h
@@ -37,15 +37,16 @@ extern "C" {
 ad_node_t *ad_par(int n_row, int n_col, const float *x);
 ad_node_t *ad_var(int n_row, int n_col, const float *x, float *d);
 
-ad_node_t *ad_add(ad_node_t *x, ad_node_t *y);
-ad_node_t *ad_sub(ad_node_t *x, ad_node_t *y);
-ad_node_t *ad_mul(ad_node_t *x, ad_node_t *y);
-ad_node_t *ad_mtmul(ad_node_t *x, ad_node_t *y);
-ad_node_t *ad_ce2(ad_node_t *x, ad_node_t *y);
+ad_node_t *ad_add(ad_node_t *x, ad_node_t *y);   // z(x,y) = x + y (element-wise/matrix addition)
+ad_node_t *ad_sub(ad_node_t *x, ad_node_t *y);   // z(x,y) = x - y (element-wise/matrix subtraction)
+ad_node_t *ad_mul(ad_node_t *x, ad_node_t *y);   // z(x,y) = x * y (element-wise product)
+ad_node_t *ad_mtmul(ad_node_t *x, ad_node_t *y); // z(x,y) = x * y^T (general matrix product, with y transposed)
+ad_node_t *ad_smul(ad_node_t *x, ad_node_t *y);  // z(x,y) = x * y; x is a scalar (scalar-to-matrix product)
+ad_node_t *ad_ce2(ad_node_t *x, ad_node_t *y);   // z(x,y) = \sum_i -y_i*log(f(x_i)) - (1-y_i)*log(1-f(x_i)); f() is sigmoid (binary cross-entropy for sigmoid)
 
-ad_node_t *ad_norm2(ad_node_t *x);
-ad_node_t *ad_sigm(ad_node_t *x);
-ad_node_t *ad_tanh(ad_node_t *x);
+ad_node_t *ad_norm2(ad_node_t *x);               // z(x) = \sum_i x_i^2 (L2 norm)
+ad_node_t *ad_sigm(ad_node_t *x);                // z(x) = 1/(1+exp(-x)) (element-wise sigmoid)
+ad_node_t *ad_tanh(ad_node_t *x);                // z(x) = (1-exp(-2x)) / (1+exp(-2x)) (element-wise tanh)
 
 ad_node_t **ad_compile(ad_node_t *root, int *n_node);
 float ad_eval(int n, ad_node_t **a);
